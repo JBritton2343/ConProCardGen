65ddba6241e882ef94f3092c8937c8909f8e07ed
JBritton2343 <jbritton854@gmail.com>
2022-07-27 21:00:33 -0500

    updated information in socials and image

--- src/js/app.js
+++ src/js/app.js
@@ -33,13 +33,13 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>James Britton</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://twitter.com/JamesBr34154243"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/Jbritton2434"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/james-britton-a37469211/"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
@@ -56,7 +56,7 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: ".\james.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
