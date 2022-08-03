47c970ac1c7e91095bad8cdb3b0b0f842bd7df6d
JBritton2343 <jbritton854@gmail.com>
2022-08-03 18:55:46 -0500

    moved avatar pic into js folder

--- src/js/app.js
+++ src/js/app.js
@@ -56,7 +56,7 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "...james.jpg",
+    avatarURL: "james.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
